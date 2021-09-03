8157b4c531619b3501d71e5aeb91af60fcd3a96c
Vitor Ferreira <vitorfer238@gmail.com>
2021-09-03 14:42:19 -0300

    fix: hide CasesFragment views if there is invalid data

--- app/src/main/java/com/vitor238/covid19brasil/ui/cases/CasesFragment.kt
+++ app/src/main/java/com/vitor238/covid19brasil/ui/cases/CasesFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.vitor238.covid19brasil.R
@@ -29,25 +30,14 @@ class CasesFragment : Fragment() {
             openDetails(view, brazilianState)
         }
 
-        return binding.root
-    }
-
-    private fun openDetails(view: View, brazilianState: BrazilianState) {
-        val intent = DetailsActivity.getIntent(requireContext(), brazilianState)
-        val options = ActivityOptions.makeSceneTransitionAnimation(
-            requireActivity(),
-            view,
-            "transitionActivity"
-        )
-        requireContext().startActivity(intent, options.toBundle())
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
         val casesViewModel = ViewModelProvider(this).get(CasesViewModel::class.java)
 
         casesViewModel.casesInBrazil.observe(viewLifecycleOwner) { brazil ->
             brazil?.let {
+                binding.materialCardConfirmed.isVisible = brazil.confirmed != "0"
+                binding.materialCardDeaths.isVisible = brazil.deaths != "0"
+                binding.materialCardRecovered.isVisible = brazil.recovered != "0"
+
                 binding.textNumberConfirmed.text = brazil.confirmed
                 binding.textRecoveredNumber.text = brazil.recovered
                 binding.textNumberDeaths.text = brazil.deaths
@@ -60,6 +50,18 @@ class CasesFragment : Fragment() {
             binding.recyclerBrazilianStates.adapter = adapterBrazilianStates
             adapterBrazilianStates.submitList(it)
         }
+
+        return binding.root
+    }
+
+    private fun openDetails(view: View, brazilianState: BrazilianState) {
+        val intent = DetailsActivity.getIntent(requireContext(), brazilianState)
+        val options = ActivityOptions.makeSceneTransitionAnimation(
+            requireActivity(),
+            view,
+            "transitionActivity"
+        )
+        requireContext().startActivity(intent, options.toBundle())
     }
 
     override fun onDestroyView() {
